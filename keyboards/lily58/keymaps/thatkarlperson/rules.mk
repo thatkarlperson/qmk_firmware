a34d756495385d1e8e80ce9c1b2ad04058e12ab1
Karl A. Krueger <that.karl.person@gmail.com>
2021-01-08 09:23:08 -0800

    remove repeated library

--- keyboards/lily58/keymaps/thatkarlperson/rules.mk
+++ keyboards/lily58/keymaps/thatkarlperson/rules.mk
@@ -20,8 +20,7 @@ SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont_lily.c \
-        ./lib/rgb_state_reader.c \
+SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
